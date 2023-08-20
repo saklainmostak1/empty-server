fd2431a2f777e5f7eb92d703d4823f3ce4e36fde
saklainmostak1 <saklain15-12003@diu.edu.bd>
2023-08-20 20:36:01 +0600

    push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.use(cors())
 app.use(express.json())
